93714006f5feb71299169d251567fe1ff1a62a7d
tgima <tgima>
2022-12-31 00:52:34 +0900

    書籍情報を自動生成に

---
+++ papers.py
@@ -0,0 +1,86 @@
+import yaml
+import datetime
+
+# 出力先ファイル
+outjp = "content/research.md"
+outen = "content/en/research.md"
+# outjp = "test/researchjp.md"
+# outen = "test/researchen.md"
+
+# データベースファイル
+publication_db = "data/publications.yml"
+
+def print_journal(x, out):
+    title = x['title'].capitalize()
+    out.write('1. ')
+    out.write(', '.join(x['author']) + '. ')
+    out.write(title + '. ')
+    out.write('_' + x['journal'] + '_, ')
+    out.write(x['volume'] + ', ')
+    out.write('pp. ' + x['pages'] + '. ')
+    out.write(x['year'] + '. ')
+    if 'doi' in x:
+        out.write("[[📖doi link]("+ x['doi'] +")]")
+    if 'arxiv' in x:
+        out.write("[[📝arXiv]("+ x['arxiv'] +")]")
+    out.write('\n')
+
+def print_conference(x, out):
+    title = x['title'].capitalize()
+    out.write('1. ')
+    # if title in jtitle:
+    #     out.write('[[Link to journal ver](#j' + jtitle[title] + ')]')
+
+    out.write(', '.join(x['author']) + '. ')
+    out.write(title + '. ')
+    out.write('_' + x['booktitle'] + '_. ')
+    out.write(x['place'] + '. ')
+    if 'series' in x:
+        out.write(x['series'] + ', ' + x['volume'] + ', pp. ' + x['pages'])
+    else:
+        out.write('accepted. ')
+    out.write(x['year'] + ', ' + x['month'] + '. ')
+
+    if 'doi' in x:
+        out.write("[[📖doi link]("+ x['doi'] +")]")
+    if 'arxiv' in x:
+        out.write("[[📝arXiv]("+ x['arxiv'] +")]")
+    out.write('\n')
+
+def print_domestic(x, out):
+    out.write('1. ')
+    # pp = x['presenter']
+    
+    out.write(', '.join(x['author']) + '. ')
+    out.write(x['title'].capitalize() + '. ')
+    out.write(', '.join(x['booktitle']) + '.\n')
+
+
+with open(outjp, mode='w') as wjp, open(outen, mode='w') as wen:
+    wjp.write(
+        '---\ntitle: "研究成果"\nsummary: "研究成果"\ndate: ' + 
+        datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S+09:00") + '\n---\n'
+    )
+    wen.write(
+        '---\ntitle: "My publications"\nsummary: "My publications"\ndate: ' + 
+        datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S+09:00") + '\n---\n'
+    )
+
+    with open(publication_db, mode='r') as cdb:
+        db = yaml.safe_load(cdb)
+        wjp.write("### Journal papers\n")
+        wen.write("### Journal papers\n")
+        for x in db["journal"]:
+            print_journal(x, wjp)
+            print_journal(x, wen)
+
+        wjp.write("### Conference papers\n")
+        wen.write("### Conference papers\n")
+        for x in db["conference"]:
+            print_conference(x, wjp)
+            print_conference(x, wen)
+
+        wjp.write("### 国内研究会発表\n")
+        for x in db["domestic"]:
+            print_domestic(x, wjp)
+
---
+++ test/researchen.md
@@ -0,0 +1,12 @@
+---
+title: "My publications"
+summary: "My publications"
+date: 2022-12-31T00:40:51+09:00
+---
+### Journal papers
+1. Yuuki Aoike, Tatsuya Gima, Tesshu Hanaka, Masashi Kiyomi, Yasuaki Kobayashi, Yusuke Kobayashi, Kazuhiro Kurita, Yota Otachi. An improved deterministic parameterized algorithm for cactus vertex deletion.Theory of Computing Systems, 66, pp. 502--515. 2022. [📖link](https://doi.org/10.1007/s00224-022-10076-x)[📝arXiv](https://arxiv.org/abs/2012.04910)
+1. Tatsuya Gima, Tesshu Hanaka, Masashi Kiyomi, Yasuaki Kobayashi, Yota Otachi. Exploring the gap between treedepth and vertex cover through vertex integrity.Theoretical Computer Science, 918, pp. 60--76. 2022. [📖link](https://doi.org/10.1016/j.tcs.2022.03.021)[📝arXiv](https://arxiv.org/abs/2101.09414)
+### Conference papers
+1. Tatsuya Gima, Yota Otachi. Extended mso model checking via small vertex integrity.The 33rd International Symposium on Algorithms and Computation (ISAAC 2022). Korea. LIPIcs, 248, pp. 20:1--20:152022, Dec. [📖link](https://doi.org/10.4230/LIPIcs.ISAAC.2022.20)[📝arXiv](https://arxiv.org/abs/2202.08445)
+1. Tatsuya Gima, Takehiro Ito, Yasuaki Kobayashi, Yota Otachi. Algorithmic meta-theorems for combinatorial reconfiguration revisited.The 30th Annual European Symposium on Algorithms (ESA 2022). Potsdam, Germany. LIPIcs, 244, pp. 61:1--61:152022, Sep. [📖link](https://doi.org/10.4230/LIPIcs.ESA.2022.61)[📝arXiv](https://arxiv.org/abs/2207.01024)
+1. Tatsuya Gima, Tesshu Hanaka, Masashi Kiyomi, Yasuaki Kobayashi, Yota Otachi.. Exploring the gap between treedepth and vertex cover through vertex integrity.The 12th International Conference on Algorithms and Complexity (CIAC 2021). Online. LNCS, 12701, pp. 271--2852021, May. [📖link](https://doi.org/10.1007/978-3-030-75242-2_19)[📝arXiv](https://arxiv.org/abs/2101.09414)
